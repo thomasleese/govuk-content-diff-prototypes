afc9c11f171be5dea04ee3e8732a8616cf4dce70
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2017-05-11 09:50:23 +0100

    Add a means of loading the example data

--- app.rb
+++ app.rb
@@ -1,5 +1,9 @@
 require 'sinatra'
 
+require_relative 'lib/data_loader'
+
+data = DataLoader.new(File.join(File.dirname(__FILE__), 'data'))
+
 get '/' do
   erb :index
 end
---
+++ lib/data_loader.rb
@@ -0,0 +1,16 @@
+class DataLoader < Hash
+  def initialize(directory)
+    super
+
+    puts "#{directory}/*/*.json"
+
+    Dir.glob("#{directory}/*/*.json") do |filename|
+      parts = filename.split(File::SEPARATOR)
+      content_id = parts[-2]
+      version = parts[-1].split('.').first.to_i
+      content = JSON.parse(File.read(filename))
+      self[content_id] = [] unless key?(content_id)
+      self[content_id][version] = content
+    end
+  end
+end
